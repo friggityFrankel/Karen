68665159a594c0272da9f7aee7425ad84d952b24
friggityFrankel <rob@robFX.com>
2024-06-24 11:53:16 -0400

    Changed sizing and key behavior, added new emotions (new images to come), refactored Neutral mode

--- Karen.py
+++ Karen.py
@@ -1,7 +1,8 @@
 import sys
 from PyQt6 import (QtCore, QtWidgets)
-from PyQt6.QtWidgets import (QMainWindow, QLabel, QWidget)
+from PyQt6.QtWidgets import (QApplication, QMainWindow, QLabel, QWidget)
 from PyQt6.QtGui import (QMovie, QPixmap)
+
 
 class ApplicationWindow(QMainWindow):
     pressedKey = 0
@@ -9,45 +10,66 @@ class ApplicationWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        screen = QApplication.primaryScreen().availableGeometry()
+
         self.setWindowTitle('K.A.R.E.N.')
-        self.setGeometry(10, 10, 720, 480)
+        self.setGeometry(0, 0, screen.width(), screen.height())
 
         self.centralwidget = QWidget(self)
         self.setCentralWidget(self.centralwidget)
 
         self.imageViewer = QLabel(self.centralwidget)
-        self.imageViewer.setGeometry(QtCore.QRect(0, 0, 720, 480))
-        image = QPixmap('images/Neutral.png')
-        self.imageViewer.setPixmap(image)
+        self.imageViewer.setGeometry(QtCore.QRect(0, 0, screen.width(), screen.height()))
         self.imageViewer.setScaledContents(True)
+        self.changeNeutral()
     
     def keyPressEvent(self, event):
-        if self.pressedKey != event.key():
+        if not event.isAutoRepeat() and self.pressedKey != event.key():
             self.pressedKey = event.key()
             self.changeImage()
+        elif not event.key() == 49:
+            self.changeNeutral()
     
     def keyReleaseEvent(self, event):
-        if not event.isAutoRepeat():
-            self.pressedKey = 0
-            self.imageViewer.setPixmap(QPixmap('images/Neutral.png'))
+        if not event.isAutoRepeat() and self.pressedKey == 49:
+            self.changeNeutral()
 
-    
+    def changeNeutral(self):
+        print('Neutral')
+        self.pressedKey = 0
+        self.imageViewer.setPixmap(QPixmap('images/Neutral.png'))
+
     def changeImage(self):
         match self.pressedKey:
-            case 49:
-                movie = QMovie('images/Waveform.gif')
-                self.imageViewer.setMovie(movie)
-                movie.start()
-            case 50:
-                self.imageViewer.setPixmap(QPixmap('images/Happy.png'))
-            case 51:
-                self.imageViewer.setPixmap(QPixmap('images/Sad.png'))
-            case 52:
-                self.imageViewer.setPixmap(QPixmap('images/Mad.png'))
-            case 53:
+            case 49: # Waveform
+                print('Waveform')
+                self.playWaveform()
+            case 50: # Maniacal augh
+                print('Maniacal laugh')
+                self.imageViewer.setPixmap(QPixmap('images/Maniacal.png'))
+            case 51: # Eye roll
+                print('Eye roll')
+                self.imageViewer.setPixmap(QPixmap('images/EyeRoll.png'))
+            case 52: # Angry
+                print('Angry')
+                self.imageViewer.setPixmap(QPixmap('images/Angry.png'))
+            case 53: # Question
+                print('Question')
                 self.imageViewer.setPixmap(QPixmap('images/Question.png'))
-            case 54:
+            case 54: # Exclamation
+                print('Exclamation')
+                self.imageViewer.setPixmap(QPixmap('images/Exclamation.png'))
+            case 55: # Love
+                print('Love')
                 self.imageViewer.setPixmap(QPixmap('images/Love.png'))
+            case 56: # Music
+                print('Music')
+                self.imageViewer.setPixmap(QPixmap('images/Music.png'))
+        
+    def playWaveform(self):
+        self.gif = QMovie('images/Waveform.gif')
+        self.imageViewer.setMovie(self.gif)
+        self.gif.start()
 
 
 if __name__ == '__main__':
